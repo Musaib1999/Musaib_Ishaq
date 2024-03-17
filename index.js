baba0cd88f4db640c7596e448c910ee6cd4e035e
Musaib Ishaq <musaib.ishaq@gmail.com>
2024-03-17 15:47:56 +0530

    added ham menu with content

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let hamMenu = document.querySelector(.ham-menu);
-hamMenu.addEventListener(click, ()=>{
-    
-})
+let hamMenu = document.querySelector(".ham-menu");
+let hamMenuContent = document.querySelector(".ham-menu-content");
+
+hamMenu.addEventListener('click', () => {
+    if (hamMenuContent.style.display === 'block' || hamMenuContent.style.display === '') {
+        hamMenuContent.style.display = 'none';
+    } else {
+        hamMenuContent.style.display = 'block';
+    }
+});
